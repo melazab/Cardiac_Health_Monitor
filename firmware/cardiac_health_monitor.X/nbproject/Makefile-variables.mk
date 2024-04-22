9f46f0363178691302987f32e030481c89751880
Mohamed Elazab <elazab.moh@gmail.com>
2024-04-21 21:47:49 -0400

    Changing project name from uart_hello_world to cardiac_health_monitor

---
+++ firmware/cardiac_health_monitor.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=cardiac_health_monitor.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/cardiac_health_monitor.X.production.hex
