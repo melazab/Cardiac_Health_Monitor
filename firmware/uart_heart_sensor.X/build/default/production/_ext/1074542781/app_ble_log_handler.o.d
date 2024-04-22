81555584e12052813a47f31e107977497b7d0e48
Mohamed Elazab <elazab.moh@gmail.com>
2024-04-21 21:41:23 -0400

    Housekeeping

--- firmware/uart_heart_sensor.X/build/default/production/_ext/1074542781/app_ble_log_handler.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/_ext/1074542781/app_ble_log_handler.o: \
- ../src/app_ble/app_ble_log_handler.c \
- ../src/app_ble/app_ble_log_handler.h
-
-../src/app_ble/app_ble_log_handler.h:
